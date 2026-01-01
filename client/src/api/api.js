c1c8c58a3c144ebe2e36955097d22d4ee613104c
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-01 16:04:32 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-assignment-2-xk3i.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
